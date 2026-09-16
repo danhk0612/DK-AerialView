5882e69697761aa945124cb089e4a32f4b2f17c6
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-16 16:58:03 +0900

    feat: add Kakao roadview reference options

--- src/DKAerialView/Models/AerialGenerationOptions.cs
+++ src/DKAerialView/Models/AerialGenerationOptions.cs
@@ -41,4 +41,10 @@ public sealed class AerialGenerationOptions
     public AerialRenderStyle RenderStyle { get; init; } = AerialRenderStyle.Realistic;
     public int TargetWidth { get; init; } = 1920;
     public int TargetHeight { get; init; } = 1080;
+
+    public bool UseKakaoRoadviewReferences { get; init; } = true;
+    public int RoadviewDistanceMeters { get; init; } = 100;
+    public int RoadviewSearchRadiusMeters { get; init; } = 100;
+    public int RoadviewTilt { get; init; } = 0;
+    public int RoadviewZoom { get; init; } = 0;
 }
